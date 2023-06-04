c6fbd3334629f018beda32e53f230a5c3ddb9ab2
Rostislav <rostislav.nagimov@gmail.com>
2023-06-03 23:17:01 -0700

    fix: remove extra data

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,10 +39,6 @@ const Profile = () => {
           {user ? (
             <div>
               <h1>Welcome {user?.username}</h1>
-              User data:
-              <pre>{JSON.stringify(user, null, 2)}</pre>
-              Eniter Web App data:
-              <pre>{JSON.stringify(webApp, null, 2)}</pre>
             </div>
           ) : (
             <div>Make sure web app is opened from telegram client</div>
