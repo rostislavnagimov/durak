97fa48dd6a70e7afbc0c4c4e29af9bf9fa2c3c72
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-06-05 17:20:49 -0700

    feat: enable profile page

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,21 +12,23 @@ const Profile = () => {
       </div>
       <div className='profile__body'>
         <div className="user">
-          {user ? (
+          <div className="user__info">
+            <div className="user__image"></div>
             <div>
-              <div className="user__image"></div>
-              <h1>{`${user?.first_name} ${user?.last_name}`}</h1>
-              <h2>{`ID: ${user?.id}`}</h2>
-              <ul>
-                <li>Сыграно игр: 000</li>
-                <li>Побед: 000</li>
-                <li>Фишек: 000</li>
-                <li>Баланс: 000</li>
-              </ul>
+              <h1>{`${user?.first_name || 'Name'} ${user?.last_name || 'Placeholder'}`}</h1>
+              <h2>{`ID: ${user?.id || 'ID'}`}</h2>
+            <ul className='user__info__stats'>
+              <li>Сыграно игр: 000</li>
+              <li>Побед: 000</li>
+              <li>Фишек: 000</li>
+              <li>Баланс: 000</li>
+            </ul>
             </div>
-          ) : (
-            <div>Make sure web app is opened from telegram client</div>
-          )}
+          </div>
+          <div>
+            <h1>Дополнительная информация</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
         </div>
         <ul>
           <li>
