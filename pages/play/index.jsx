dd9edeffe6e022b9e082cb9d9ee27a5860a2d185
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-06-08 13:55:46 -0700

    feat: fix cards

--- pages/play/index.jsx
+++ pages/play/index.jsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 const Play = () => {
-  let deck = []
-  if (deck.length === 0) {
-    let i = 0
-    while (i < 6) {
-      let number = Math.floor(Math.random() * (49 - 11 + 1)) + 11
-      if (String(number).includes('0')) {
-        deck.push(number + 1)
-      } else {
-        deck.push(number)
-      }
-      i++
-    }
+  const fullDeck = [11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  const getRandomNumbers = () => {
+    let randomNumbers = [];
+
+    while (randomNumbers.length < 6) {
+      let randomNumber = getRandomNumber(0, fullDeck.length - 1);
+
+      if (!randomNumbers.includes(fullDeck[randomNumber])) {
+        randomNumbers.push(fullDeck[randomNumber]);
+      }
+    }
+
+  return randomNumbers;
+}
+ let deck =[]
+ if (deck.length === 0) {
+  deck = getRandomNumbers()
+ }
+
 
   return (
     <>
