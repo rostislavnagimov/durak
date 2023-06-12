8b6dc8ebdb208bc3fb15480fbb4ee7f4f5653faa
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-06-12 13:26:36 -0700

    feat: make friends

--- pages/friends/index.jsx
+++ pages/friends/index.jsx
@@ -1,18 +1,80 @@
-import Link from "next/link";
-import { useTelegram } from "../../telegram";
+
+const friends = [
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+  {
+    name: 'Friend Friendovich',
+    status: 'Online',
+    games: 10,
+    wins: 5,
+    id: 144415
+  },
+]
 
 
 const Friends = () => {
-  const { user, webApp } = useTelegram()
   return (
   <>
     <div className='profile'>
-        <p className='profile__text'>START</p>
+        <p className='profile__text'>FRIENDS</p>
     </div>
     <div className='profile__body'>
-      {user && Object.keys(webApp?.initData).map((i) => (
-        <p>{user[i]}</p>
-      ))}
+      <div className='friends'>
+        {friends.map((friend) => (
+          <div className='friend' key={friend.id}>
+            <div className='friend__image__small'></div>
+            <div className="friend__info">
+              <h1>{friend.name}</h1>
+              <h2>{friend.status}</h2>
+              <span>{`Games: ${friend.games} / Wins: ${friend.wins}`}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="invite">
+        <button>SEND LINK</button>
+
+      </div>
     </div>
   </>
 )}
